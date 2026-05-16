419bdcb457638ce3efdebe7b726faa8e9d7737d3
possior <153281819+possior@users.noreply.github.com>
2026-05-17 01:14:17 +0900

    20260517 | Modified /installer.sh

--- installer.sh
+++ installer.sh
@@ -71,5 +71,6 @@ fi
 if
   [[ "${arguments[o]}" == "endeavour" ]]
 then
-  echo "this is placeholder"
+  install -y "$ROOT/source/.omegat/omegat.prefs" "$HOME/.omegat/omegat.prefs"
+  install -y "$ROOT/source/.omegat/uiLayout.xml" "$HOME/.omegat/uiLayout.xml"
 fi
