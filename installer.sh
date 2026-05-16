2e9e63b80882ac970aaca2348d2193bcabe69612
possior <153281819+possior@users.noreply.github.com>
2026-05-16 21:45:46 +0900

    20260516 | Modified /installer.sh

--- installer.sh
+++ installer.sh
@@ -1,3 +1,21 @@
+declare -A arguments
+
+arguments[default]=0
+
+while
+  [[ "$#" -gt 0 ]]
+do
+  case "$1" in
+    -d | --default)
+      arguments[default]=1
+      shift 1
+      ;;
+    *)
+      exit
+      ;;
+  esac
+done
+
 if
   [[ -n "$(git rev-parse --show-toplevel) 2>/dev/null)" ]]
 then
