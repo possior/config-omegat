1d803b6b70a88fef07db289cb396d5bcad5aa651
possior <153281819+possior@users.noreply.github.com>
2026-05-16 21:20:05 +0900

    20260516 | Modified /installer.sh

--- installer.sh
+++ installer.sh
@@ -1,2 +1,3 @@
 [[ -n "$(git rev-parse --show-toplevel 2>/dev/null)" ]] && ROOT="$(git rev-parse --show-toplevel)" || exit
 [[ -f "$ROOT/script/utility.sh" ]] && source "$ROOT/script/utility.sh" || exit
+[[ -d "$HOME/.omegat" ]] && mkdir -p "$HOME/.omegat"
