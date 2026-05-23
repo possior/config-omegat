b7883e592e84cdd604be664a820e277ef4ab7d1d
possior <153281819+possior@users.noreply.github.com>
2026-05-23 22:57:17 +0900

    20260523 | Modified /installer-curl.sh

--- installer-curl.sh
+++ installer-curl.sh
@@ -1,12 +1,22 @@
-repository="https://github.com/possior/config-omegat.git"
 directory="$HOME/.omegat"
 temporary="$(mktemp -d)"
+repository="https://github.com/possior/config-omegat.git"
+system=""
 version="default"
-
 while
   [[ "$#" -gt 0 ]]
 do
   case "$1" in
+    -o | --os | --operating-system | --system)
+      if
+        [[ -z "$2" || "$2" =~ ^- ]]
+      then
+        echo ":: ERROR :: $1 requires a value"
+        exit 1
+      fi
+      system="$2"
+      shift 2
+      ;;
     -v | --ver | --version)
       if
         [[ -z "$2" || "$2" =~ ^- ]]
@@ -17,9 +27,12 @@ do
       version="$2"
       shift 2
       ;;
+    *)
+      echo ":: ERROR :: $1 is not a valid argument"
+      exit 1
+      ;;
   esac
 done
-
 git clone "$repository" "$temporary" -b "$version"
 cd "$temporary"
 rm -rf "$temporary"
