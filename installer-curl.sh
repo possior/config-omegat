80a12a939193b7648b68a517167bd9caa61a4831
possior <153281819+possior@users.noreply.github.com>
2026-05-23 22:44:19 +0900

    20260523 | Modified /installer-curl.sh

--- installer-curl.sh
+++ installer-curl.sh
@@ -1,4 +1,25 @@
-gith="https://github.com/possior/config-omegat.git"
-repo="$(mktemp -d)"
-git clone "https://github.com/possior/config-omegat.git" "$repo"
-cd "$repo"
+repository="https://github.com/possior/config-omegat.git"
+directory="$HOME/.omegat"
+temporary="$(mktemp -d)"
+version="default"
+
+while
+  [[ "$#" -gt 0 ]]
+do
+  case "$1" in
+    -v | --ver | --version)
+      if
+        [[ -z "$2" || "$2" =~ ^- ]]
+      then
+        echo ":: ERROR :: $1 requires a value"
+        exit 1
+      fi
+      version="$2"
+      shift 2
+      ;;
+  esac
+done
+
+git clone "$repository" "$temporary" -b "$version"
+cd "$temporary"
+rm -rf "$temporary"
